f62f4b741bf0fee63e5c8e264d7291798dc13da6
lucas carvalho <lucas.araripina@hotmail.com>
2024-08-21 15:40:48 -0300

    adicionando cache em lancamentos

--- src/shared/cache/RedisCache.ts
+++ src/shared/cache/RedisCache.ts
@@ -27,16 +27,4 @@ export default class RedisCache {
   public async invalidate(key: string): Promise<void> {
     await this.client.del(key);
   }
-
-  public async invalidatePrefix(prefix: string): Promise<void> {
-    const keys = await this.client.keys(`${prefix}:*`);
-
-    const pipeline = this.client.pipeline();
-
-    keys.forEach(key => {
-      pipeline.del(key);
-    });
-
-    await pipeline.exec();
-  }
 }
